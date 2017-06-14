4e7ceec8fb376140f87b8d08d14f900dc7915f0e
zhouyong <zhouyong@chinac.com>
2017-06-14 21:52:00 +0800

    ui polish

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -22,11 +22,21 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div className="ws-home">
-        <Link to="/theme">专题</Link>
-        <Link to="/intro">产品介绍</Link>
-        <Link to="/about">关于我们</Link>
-        <Link to="/contact">联系我们</Link>
-        <Link to="/join">招商加盟</Link>
+        <div className="ws-banner">
+          五之行
+        </div>
+        <div className="ws-box theme">
+          <Link to="/theme">专题</Link>
+        </div>
+        <div className="ws-box product">
+          <Link to="/intro">产品介绍</Link>
+        </div>
+        <div className="ws-box about">
+          <Link to="/about">关于我们</Link>
+        </div>
+        <div className="ws-box join">
+          <Link to="/join">招商加盟/人才招聘</Link>
+        </div>
       </div>
     );
   }
